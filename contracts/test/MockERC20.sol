154c47a2a3687c8a662d5c5f81dce2da08f8da48
highsoft-85 <pingan@chatandbuild.com>
2025-07-24 18:02:22 +0200

    fix: rename the constructor params of MockERC20 contract to avoid conflict ERC20

--- contracts/test/MockERC20.sol
+++ contracts/test/MockERC20.sol
@@ -9,10 +9,10 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
  */
 contract MockERC20 is ERC20 {
     constructor(
-        string memory name,
-        string memory symbol,
+        string memory _name,
+        string memory _symbol,
         uint256 initialSupply
-    ) ERC20(name, symbol) {
+    ) ERC20(_name, _symbol) {
         _mint(msg.sender, initialSupply);
     }
 
