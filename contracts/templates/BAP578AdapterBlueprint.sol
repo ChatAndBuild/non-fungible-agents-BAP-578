976dbe16838d695ad3ee5f30ce3622125200559c
DevTeam <noreply@private.dev>
2026-03-07 09:59:19 +0800

    fix(template): harden adapter skeleton against reentrancy scanner findings

--- contracts/templates/BAP578AdapterBlueprint.sol
+++ contracts/templates/BAP578AdapterBlueprint.sol
@@ -13,6 +13,7 @@ interface IAgentVault {
 contract BAP578AdapterBlueprint {
     address public immutable nfa;
     IAgentVault public immutable vault;
+    uint256 private _locked;
 
     event NativeFunded(uint256 indexed tokenId, address indexed sender, uint256 amount);
     event NativeWithdrawn(uint256 indexed tokenId, address indexed recipient, uint256 amount);
@@ -22,22 +23,29 @@ contract BAP578AdapterBlueprint {
         _;
     }
 
+    modifier nonReentrant() {
+        require(_locked == 0, "ReentrancyGuard");
+        _locked = 1;
+        _;
+        _locked = 0;
+    }
+
     constructor(address nfa_, address vault_) {
         require(nfa_ != address(0) && vault_ != address(0), "Invalid address");
         nfa = nfa_;
         vault = IAgentVault(vault_);
     }
 
-    function fund(uint256 tokenId) external payable {
+    function fund(uint256 tokenId) external payable nonReentrant {
         require(msg.value > 0, "Amount must be > 0");
-        vault.creditNative{ value: msg.value }(tokenId);
         emit NativeFunded(tokenId, msg.sender, msg.value);
+        vault.creditNative{ value: msg.value }(tokenId);
     }
 
-    function withdraw(uint256 tokenId, uint256 amount, address to) external onlyOperator(tokenId) {
+    function withdraw(uint256 tokenId, uint256 amount, address to) external onlyOperator(tokenId) nonReentrant {
         require(to != address(0), "Invalid recipient");
         require(amount > 0, "Amount must be > 0");
-        vault.debitNative(tokenId, amount, to);
         emit NativeWithdrawn(tokenId, to, amount);
+        vault.debitNative(tokenId, amount, to);
     }
 }
